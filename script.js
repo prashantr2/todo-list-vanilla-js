79f3f57626c5b96fdfa43a6c14bcfed24fd7315f
prashantr2 <prashantrawat2ofc@gmail.com>
2024-10-21 00:59:11 +0530

    Added desc as well

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
 const todoForm = document.querySelector('#todoForm');
-const todoInput = document.querySelector('#todoInput');
+const todoInputTitle = document.querySelector('#todoInputTitle');
+const todoInputDesc = document.querySelector('#todoInputDesc');
 const todoTable = document.querySelector('#todoTable');
 
 todoForm.addEventListener('submit', (e) => {
     e.preventDefault();
     let newTodo = document.createElement('div');
-    let todoText = document.createElement('span');
-    todoText.innerText = todoInput.value;
+    let todoTitle = document.createElement('span');
+    let todoDesc = document.createElement('span');
+    todoTitle.innerText = todoInputTitle.value;
+    todoDesc.innerText = todoInputDesc.value;
+    todoDesc.style.color = '#777';
     let todoButton = document.createElement('button');
     todoButton.innerText = 'Mark as Done';
     todoButton.addEventListener('click', () => {
-        todoText.style.textDecoration = 'line-through';
+        todoTitle.style.textDecoration = 'line-through';
+        todoDesc.style.textDecoration = 'line-through';
         todoButton.style.display = 'none';
     })
-    newTodo.appendChild(todoText);
+    newTodo.appendChild(todoTitle);
+    newTodo.appendChild(todoDesc);
     newTodo.appendChild(todoButton);
 
-    todoInput.value = '';
+    todoInputTitle.value = '';
+    todoInputDesc.value = '';
     let firstRowElement = document.createElement('td');
-    firstRowElement.appendChild(todoText);
+    firstRowElement.appendChild(todoTitle);
     let secondRowElement = document.createElement('td');
-    secondRowElement.appendChild(todoButton);
+    secondRowElement.appendChild(todoDesc);
+    let thirdRowElement = document.createElement('td');
+    thirdRowElement.appendChild(todoButton);
 
     let tableRow = document.createElement('tr');
     tableRow.appendChild(firstRowElement);
     tableRow.appendChild(secondRowElement);
+    tableRow.appendChild(thirdRowElement);
     todoTable.appendChild(tableRow);
 })
